aa4ad504ae2ba4b92e1df399711bac4f08e76f17
Fernando Rivera <fernando.rivera0011@gmail.com>
2022-11-21 13:58:52 -0600

    fixed js file

--- js/external.js
+++ js/external.js
@@ -1,5 +1,7 @@
+"use strict";
 console.log("Hello from external JavaScript");
-alert('Welcome to my Website!');
-let usercolor = prompt('What Is your favorite color?');
-let message = "Great, " + usercolor + " is my favorite color too!");
+alert("Welcome to my Website!");
+let usercolor = prompt("What's your favorite color?");
+console.log("Users favorite color is " + usercolor + ".");
+let message = "Great, " + usercolor + " is my favorite color too!";
 alert(message);
