149c6ec801edb8b97027d86d1553512103d36fed
Fernando Rivera <fernando.rivera0011@gmail.com>
2022-11-21 13:42:17 -0600

    copied from javascript-with-html

---
+++ js/external.js
@@ -0,0 +1,5 @@
+console.log("Hello from external JavaScript");
+alert('Welcome to my Website!');
+let usercolor = prompt('What Is your favorite color?');
+let message = "Great, " + usercolor + " is my favorite color too!");
+alert(message);
