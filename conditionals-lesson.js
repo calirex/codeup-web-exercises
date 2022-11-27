6d364cb6ef440e16fc3c1a977735e3f9a7d67781
Fernando Rivera <fernando.rivera0011@gmail.com>
2022-11-27 14:25:47 -0600

    Worked on problems 1-2

---
+++ conditionals-lesson.js
@@ -0,0 +1,40 @@
+let location = "San Antonio";
+if (location == "San Antonio"){
+    console.log("The condition is True" The location is Boston.");
+} else {
+    console.log("The condition is false");
+}
+
+let pizzaPrefrence = prompt("What is your favorite kind of pizza?");
+if ( pizzaPrefrence == "cheese" || pizzaPrefrence == "pepperoni"){
+    console.log("Thanks Kevin from Home Alone!");
+    else if (pizzaPrefrence.toLowerCase() == "Pineapples And Hot sauce."
+    )
+}
+
+let message = success ? "The operation was succesful" : "The operation was not succesful";
+console.log(pizza);
+
+if (condition) {
+} else if (condition2) {
+
+} else {
+
+}
+switch(pizzaPrefrence){
+    case "cheese":
+        console.log("Thanks Kevin from Home Alone");
+        break;
+    case "Pineapples and hot sauce":
+        console.log("what a coincidence, I like pineapples and hot sauce too!");
+        break;
+    default:
+        console.log("Oh, okay... that's cool... my aunt's vegan too...");
+        break;
+}
+
+let number = 2;
+switch(number){
+    case1:
+        console.log('The number is ${number}')
+}
