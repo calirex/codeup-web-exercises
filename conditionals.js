52aaef2ccfb8efff80d0e07879f22c612a847955
Fernando Rivera <fernando.rivera0011@gmail.com>
2022-11-29 16:00:07 -0600

    Completed Exercises

--- conditionals.js
+++ conditionals.js
@@ -21,20 +21,35 @@
  * Test your function by passing various string literals to it and
  * console.logging the function's return value
  */
- let color = "blue"
- console.log(color)
+// let color = "blue"
+ //console.log(color)
 
- analyzeColor(color);
+ //analyzeColor(color);
 
- function analyzeColor(color){
-     if (color === "blue") {
-         console.log("Blue is the color of the ocean.");
-     } else if  (color === "red") {
-         console.log("Clowns have red noses.");
-     } else {
-         console.log(`I don't know anything about ${color}.`);
-     }
- }
+ //function analyzeColor(color){
+    // if (color === "blue") {
+         //console.log("Blue is the color of the ocean.");
+     //} else if  (color === "red") {
+        // console.log("Clowns have red noses.");
+     //} else {
+         //console.log(`I don't know anything about ${color}.`);
+     //}
+ //}
+
+// function analyzeColor(color){
+//      let colorLower = color.toLowerCase();
+//      if(colorLower === "blue"){
+//          return "blue is the color of the sky";
+//      } else if(colorLower === "red"){
+//          return "Strawberries are Red";
+//      } else if(colorLower === "orange"){
+//          return "pumpkins are orange";
+//      } else {
+//          return "I don't know what the color is";
+//      }
+// }
+// let result = analyzeColor("Red");
+// console.log(result);
 
 // Don't change the next two lines!
 // These lines create two variables for you:
@@ -48,30 +63,50 @@ var randomColor = colors[Math.floor(Math.random() * colors.length)];
  * Pass the `randomColor` variable to your 'analyzeColor' function and console.log the results.
  * You should see a different message every time you refresh the page
  */
- analyzeColor(randomColor);
- console.log(analyzeColor(randomColor));
+let result2 = analyzeColor(randomColor);
+console.log(result2);
+
+ //analyzeColor(randomColor);
+ //console.log(analyzeColor(randomColor));
 /**
  * TODO:
  * Comment out the code above, and refactor your function to use a switch-case statement
  */
- var analyzeColor = randomColor;
- console.log(randomColor);
+function analyzeColor(color){
+    let colorLower = color.toLowerCase();
+    switch(colorLower){
+        case "blue":
+            return "blue is the color of the sky";
+            break;
+        case "red":
+            return "Strawberries are Red";
+            break;
+        case "orange":
+            return "pumpkins are orange";
+            break;
+        default:
+            break;
+    }
+}
 
- switch (analyzeColor) {
-     case "blue":
-         console.log("Blue is the color of the ocean.");
-         break;
-     case "red":
-         console.log("Clowns have a red nose");
-         break;
-     case "yellow":
-         console.log("Sunflowers are yellow.");
-         break;
-     case "green":
-         console.log("Green is the color of my grass.")
-         break;
-     default:
-         console.log("I dont know anything about that color.")
+ // var analyzeColor = randomColor;
+ // console.log(randomColor);
+ //
+ // switch (analyzeColor) {
+ //     case "blue":
+ //         console.log("Blue is the color of the ocean.");
+ //         break;
+ //     case "red":
+ //         console.log("Clowns have a red nose");
+ //         break;
+ //     case "yellow":
+ //         console.log("Sunflowers are yellow.");
+ //         break;
+ //     case "green":
+ //         console.log("Green is the color of my grass.")
+ //         break;
+ //     default:
+ //         console.log("I dont know anything about that color.")
  }
 
 /**
@@ -80,24 +115,28 @@ var randomColor = colors[Math.floor(Math.random() * colors.length)];
  * user to your `analyzeColor` function. Alert the return value from your
  * function to show it to the user.
  */
- var analyzeColor = prompt("what is your favorite color?");
- switch (analyzeColor) {
-     case "blue":
-         alert("Blue is the color of the ocean.");
-         break;
-     case "red":
-        alert("Clowns have a red nose");
-         break;
-     case "yellow":
-         alert("Sunflowers are yellow.");
-         break;
-     case "green":
-         alert("Green is the color of my grass.");
-         break;
-     default:
-         alert("I dont know anything about that color.");
-         break;
- }
+let userColor = prompt("What is your favorite color?");
+let userColorResult = analyzeColor(userColor);
+console.log(userColorResult);
+
+ // var analyzeColor = prompt("what is your favorite color?");
+ // switch (analyzeColor) {
+ //     case "blue":
+ //         alert("Blue is the color of the ocean.");
+ //         break;
+ //     case "red":
+ //        alert("Clowns have a red nose");
+ //         break;
+ //     case "yellow":
+ //         alert("Sunflowers are yellow.");
+ //         break;
+ //     case "green":
+ //         alert("Green is the color of my grass.");
+ //         break;
+ //     default:
+ //         alert("I dont know anything about that color.");
+ //         break;
+ // }
 
 /* ########################################################################## */
 
@@ -121,31 +160,62 @@ var randomColor = colors[Math.floor(Math.random() * colors.length)];
  * return value.
  */
 
- let luckyNumber = Math.floor((Math.random() * 6) + 0);
- console.log(`The random discount number is ${luckyNumber}`);
- let userAmount = prompt(`What is the total amount of your bill?`);
- console.log(`The user's bill is ${userAmount}`);
- console.log(`Your new discounted total is ${calculateTotal(luckyNumber, userAmount)}.`);
+function calculateTotal(luckyNum, total){
+    let discount,
+        discountedPrice;
+    switch (luckyNum) {
+        case 0:
+            discount = 0;
+            break;
+        case 1:
+            discount = 0.1;
+            break;
+        case 2:
+            discount = 0.25;
+            break;
+        case 3:
+            discount = 0.35;
+            break;
+        case 4:
+            discount = 0.5;
+            break;
+        case 5:
+            discount = 1;
+            break:
+        default:
+            return "Incorrect lucky Number Provided";
+            break;
+    }
+    if ( !isNaN(Total) ){
+        discountedPrice = (total * (1 - discounted));
+    } else {
+        return "the Total Provided is not a Number stupid!";
+    }
+    return discountedPrice;
+}
 
- function calculateTotal(luckyNumber, userAmount) {
+ // let luckyNumber = Math.floor((Math.random() * 6) + 0);
+ // console.log(`The random discount number is ${luckyNumber}`);
+ // let userAmount = prompt(`What is the total amount of your bill?`);
+ // console.log(`The user's bill is ${userAmount}`);
+ // console.log(`Your new discounted total is ${calculateTotal(luckyNumber, userAmount)}.`);
 
-     if (luckyNumber === 0) {
-         return `Sorry no discount`;
-     } else if (luckyNumber === 1) {
-         return "$" + (.9 * userAmount);
-     } else if (luckyNumber === 2) {
-         return "$" + (.75 * userAmount);
-     } else if (luckyNumber === 3) {
-         return "$" + (.65 * userAmount);
-     } else if (luckyNumber === 4) {
-         return "$" + (.5 * userAmount);
-     } else {
-         return `Congratulations, you get everything for FREE!!!`
-     }
- }
+ //function calculateTotal(luckyNumber, userAmount) {
 
-
-
+ //    if (luckyNumber === 0) {
+ //return `Sorry no discount`;
+ //    } else if (luckyNumber === 1) {
+ //        return "$" + (.9 * userAmount);
+ //    } else if (luckyNumber === 2) {
+ //        return "$" + (.75 * userAmount);
+ //    } else if (luckyNumber === 3) {
+ //        return "$" + (.65 * userAmount);
+ //    } else if (luckyNumber === 4) {
+ //        return "$" + (.5 * userAmount);
+ //    } else {
+ //        return `Congratulations, you get everything for FREE!!!`
+ //    }
+ //}
 /**
  * TODO:
  * Uncomment the line below to generate a random number between 0 and 5.
@@ -154,32 +224,37 @@ var randomColor = colors[Math.floor(Math.random() * colors.length)];
  * and alerts to display to the user what their lucky number was, what their
  * price before the discount was, and what their price after the discount is.
  */
- Generate a random number between 0 and 6
+//Generate a random number between 0 and 6
  var luckyNumber = Math.floor(Math.random() * 6);
+ let userNumberPrompt = prompt("What was the total of your bill?");
+ let userResult2 = calculateTotal(luckyNumber, userNumberPrompt);
+ alert(`Your lucky number Was ${luckyNumber}.
+ The price before the discount was $${userNumberPrompt}.
+ The price after the discount is $${userResult2}`);
 
- let luckyNumber = 1;
- console.log(`The random discount number is ${luckyNumber}`);
- let userAmount = prompt(`What is the total amount of your bill?`);
- console.log(`The user's bill is ${userAmount}`);
-
- calculateTotal();
-
- function calculateTotal(luckyNumber, userAmount) {
-
-     if (luckyNumber === 0) {
-         alert(`Your lucky number was 0, Sorry no discount`);
-     } else if (luckyNumber === 1) {
-         alert("Your lucky number was 1, your new discounted price is $" + (.9 * userAmount));
-     } else if (luckyNumber === 2) {
-         alert("Your lucky number was 2, your new discounted price is $" + (.75 * userAmount));
-     } else if (luckyNumber === 3) {
-         alert("Your lucky number was 3, your new discounted price is $" + (.65 * userAmount));
-     } else if (luckyNumber === 4) {
-         alert("Your lucky number was 4, your new discounted price is $" + (.5 * userAmount));
-     } else {
-         alert(`Your lucky number was 5, Congratulations, you get everything for FREE!!!`);
-     }
- }
+ // let luckyNumber = 1;
+ // console.log(`The random discount number is ${luckyNumber}`);
+ // let userAmount = prompt(`What is the total amount of your bill?`);
+ // console.log(`The user's bill is ${userAmount}`);
+ //
+ // calculateTotal();
+ //
+ // function calculateTotal(luckyNumber, userAmount) {
+ //
+ //     if (luckyNumber === 0) {
+ //         alert(`Your lucky number was 0, Sorry no discount`);
+ //     } else if (luckyNumber === 1) {
+ //         alert("Your lucky number was 1, your new discounted price is $" + (.9 * userAmount));
+ //     } else if (luckyNumber === 2) {
+ //         alert("Your lucky number was 2, your new discounted price is $" + (.75 * userAmount));
+ //     } else if (luckyNumber === 3) {
+ //         alert("Your lucky number was 3, your new discounted price is $" + (.65 * userAmount));
+ //     } else if (luckyNumber === 4) {
+ //         alert("Your lucky number was 4, your new discounted price is $" + (.5 * userAmount));
+ //     } else {
+ //         alert(`Your lucky number was 5, Congratulations, you get everything for FREE!!!`);
+ //     }
+ // }
 
 
 
@@ -202,23 +277,60 @@ var randomColor = colors[Math.floor(Math.random() * colors.length)];
  * HINT: The way we prompt for a value could be improved
  */
 
-let result = confirm("Would you like to enter a number?");
-if (result == true) {
-    let userNumber = prompt("What number would you like to enter?");
-    if (userNumber % 2 == 0) {
-        alert("Your number is even.");
+let userConfirm = confirm("Would you like to eneter a number?");
+let userNumber;
+if (userConfirm) {
+    userNumber = prompt("Please enter a number");
+    if ( !isNaN(userNumber)){
+
+    if (isEven(userNumber) )
+    alert('The number is even');
+} else {
+    alert('The number is odd');
+}
+    let plus100 = parseFloat(userNumber) + 100;
+    alert ('The number plus 100 equals: ' + plus100);
+
+    if(usernumber > 0 ) {
+        alert('The number is positive');
     } else {
-        alert("Your number is odd.");
-    }
-    alert("Your number plus 100 equals " + (parseFloat(userNumber) + 100));
-    if (userNumber > 0) {
-        alert("Your number is positive.");
-    } else {
-        alert("Your number is negative.");
+        alert('The number is negative');
     }
 } else {
-    console.log("User didn't want to enter a number");
+    alert('Incorrect data type provided.');
 }
+}
+
+function isEven(num){
+    return num % 2 === 0;
+}
+function add100(num) {
+    return parseFloat(num) + 100;
+}
+
+function isPositive(num){
+    return num > 0;
+}
+
+
+
+// let result = confirm("Would you like to enter a number?");
+// if (result == true) {
+//     let userNumber = prompt("What number would you like to enter?");
+//     if (userNumber % 2 == 0) {
+//         alert("Your number is even.");
+//     } else {
+//         alert("Your number is odd.");
+//     }
+//     alert("Your number plus 100 equals " + (parseFloat(userNumber) + 100));
+//     if (userNumber > 0) {
+//         alert("Your number is positive.");
+//     } else {
+//         alert("Your number is negative.");
+//     }
+// } else {
+//     console.log("User didn't want to enter a number");
+// }
 
 
 
---
+++ js/function-drills-exercise.js
@@ -0,0 +1,43 @@
+// ================ FUNCTION DRILLS
+
+
+// 1) Make a function called returnTwo() that returns the number 2 when called
+// ---Test this function with console.log(returnTwo())
+function returnTwo(){
+    return 2;
+}
+console.log(returnTwo())
+
+// 2) Make a function called returnName() that returns the string of your name
+// ---Test this function with console.log(returnName())
+function returnName(){
+    return "Fernando";
+}
+console.log(returnName())
+
+// 3) Make a function called addThree() which takes in a number input and returns the number plus 3.
+// ---Test this function with console.log(addThree(5))
+function addThree(num){
+    return num + 3;
+}
+console.log(addThree(5))
+
+// 4) Make a function called sayString() which returns the string input passed in.
+// ---Test this function with console.log(sayString())
+function sayString(){
+    return "blah";
+}
+console.log(sayString())
+
+// 5) Make a function called sayHowdy() which console.logs the string "Howdy!"
+// ---Test this function by directly calling sayHowdy()
+// ---Remember this function does not need a defined return value
+function sayHowdy(){
+
+}
+console.log("Howdy!")
+// ======== CONDITIONALS (switch / ternary / with functions)
+
+
+// write a function, abbrevToDay, that takes in a three-character abbreviation for the day of the week and returns the complete day of the week
+// Example: abbrevToDay('mon') returns 'Monday'
