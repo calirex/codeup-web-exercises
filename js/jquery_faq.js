48c545dac3dfb9bda968cfb7567f93f527d8eb73
Fernando Rivera <fernando.rivera0011@gmail.com>
2023-01-18 15:50:26 -0600

    Finished jquery exercise

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,50 +1,20 @@
-// Q-CLICK LISTENERS//
-// $("dt#q1").on("click", function(){
-//     $("#a1").toggleClass("invisible")
-// });
-//
-// $("dt#q2").on("click", function(){
-//     $("#a2").toggleClass("invisible")
-// });
-//
-// $("dt#q3").on("click", function(){
-//     $("#a3").toggleClass("invisible")
-// });
-//
-// $("dt#q4").on("click", function(){
-//     $("#a4").toggleClass("invisible")
-// });
-//
-// $("dt#q5").on("click", function(){
-//     $("#a5").toggleClass("invisible")
-// });
-//
-// $("dt#q6").on("click", function(){
-//     $("#a6").toggleClass("invisible")
-// });
-//
-// $("dt#q7").on("click", function(){
-//     $("#a7").toggleClass("invisible")
-// });
-//
-// $("dt#q8").on("click", function(){
-//     $("#a8").toggleClass("invisible")
-// });
-//
-// $("dt#q9").on("click", function(){
-//     $("#a9").toggleClass("invisible")
-// });
-//
-// $("dt#q10").on("click", function(){
-//     $("#a10").toggleClass("invisible")
-// });
-//
-//
-// // HIGHLIGHT LISTENERS//
-// $("dt").on("click", function(){
-//     $(this).toggleClass("highlight")
-// });
+$(document).ready(function () {
 
-$(`dt`).on(`click`, function(){
-    $(this).toggleClass(`highlight`).next().toggleClass(`invisible`);
+    $(`dt`).on(`click`, function () {
+        $(this).toggleClass(`highlight`).next().toggleClass(`invisible`);
+    });
+
+    $(`#yellowBtn`).on(`click`, function(){
+        $(`ul`).each(function(){
+            $(this).children(`li`).last().toggleClass(`highlight`);
+        });
+    });
+    $("h3").on("click", function(){
+        $(this).next().children().toggleClass("fontBold");
+    });
+    $("li").on("click", function(){
+        $(this).parent().children().first().toggleClass("blue");
+        })
 });
+
+
