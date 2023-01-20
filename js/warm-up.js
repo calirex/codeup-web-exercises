f8c0f5b4be6a1e63f48ce14898a6e184c924c21b
Fernando Rivera <fernando.rivera0011@gmail.com>
2023-01-20 09:43:57 -0600

    Finished jquery faq

--- js/warm-up.js
+++ js/warm-up.js
@@ -116,70 +116,69 @@
 
 // example data...
 
-function outcome (neighborhood){
-if ((neighborhood1, neighborhood2, neighborhood3 < 300000)) {        // logical condition
-    status = "Eligible.";               // executed if condition is true
-} else {                                // else block is optional
-    status = "Not eligible.";           // executed if condition is false
-}
-}
+//
 
-const neighborhood1 = {
-    neighborhood: "Lovely Estates",
-    medianHomePrice: 280000,
-    pool: true,
-    tennis: false,
-    crimeRate: "low",
-    schools: [
-        {name: "Elementary School", rating: 8},
-        {name: "Middle School", rating: 6},
-        {name: "High School", rating: 8}
-    ]
-};
+// Create a function, encodeStr, that takes in a string and returns the string of characters with the following substitutions:
 
-const neighborhood2 = {
-    neighborhood: "Luminous Estates",
-    medianHomePrice: 270000,
-    pool: true,
-    tennis: false,
-    crimeRate: "high",
-    schools: [
-        {name: "Elementary School", rating: 8},
-        {name: "Middle School", rating: 8},
-        {name: "High School", rating: 8}
-    ]
+function removeA2 (input){
+    let lowered = input.toLowerCase();
+    lowered = lowered.replaceAll('a', '@');
+    lowered = lowered.replaceAll('i', '1');
+    lowered = lowered.replaceAll('s', '$');
+    return lowered;
 }
 
-const neighborhood3 = {
-    neighborhood: "Oak Mountain",
-    medianHomePrice: 290000,
-    pool: false,
-    tennis: false,
-    crimeRate: "low",
-    schools: [
-        {name: "Elementary School", rating: 8},
-        {name: "Middle School", rating: 8},
-        {name: "High School", rating: 8}
-    ]
+function encodeStr(str) {
+    let encodedStr = '';
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        if (char === 'a') {
+            encodedStr += '1';
+        } else if (char === 'e') {
+            encodedStr += '2';
+        } else if (char === 'i') {
+            encodedStr += '3';
+        } else if (char === 'o') {
+            encodedStr += '4';
+        } else if (char === 'u') {
+            encodedStr += '5';
+        } else {
+            encodedStr += char;
+        }
+    }
+    return encodedStr;
 }
 
-const neighborhood3 = {
-    neighborhood: "Ginormous Acres",
-    medianHomePrice: 350000,
-    pool: true,
-    tennis: true,
-    crimeRate: "low",
-    schools: [
-        {name: "Elementary School", rating: 9},
-        {name: "Middle School", rating: 9},
-        {name: "High School", rating: 9}
-    ]
+/* ‘a’ or ‘A’ becomes ‘@’ */
+function encodeStr(str) {
+    let encodedStr = '';
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        if (char === 'a' || char === 'A') {
+            encodedStr += '@';
+        } else if (char === 'e') {
+            encodedStr += '2';
+        } else if (char === 'i') {
+            encodedStr += '3';
+        } else if (char === 'o') {
+            encodedStr += '4';
+        } else if (char === 'u') {
+            encodedStr += '5';
+        } else {
+            encodedStr += char;
+        }
+    }
+    return encodedStr;
 }
 
-fn(neighborhood1) // returns false due to school rating
-fn(neighborhood2) // returns false due to crime rate
-fn(neighborhood3) // returns true
-fn(neighborhood4) // returns false due to median home price
+// ‘a’ or ‘A’ becomes ‘@’
+// ‘i’ or ‘I’ becomes ‘1’
+// ‘s’ or ‘S’ becomes ‘$’
+//
+// encodeStr(‘apple’) // returns ‘@pple’
+// encodeStr(‘codeup’) // returns ‘codeup’
+// encodeStr(‘SASS’) // returns ‘$@$$’
+// encodeStr(‘bike’) // returns ‘b1ke’
 
 
 
