78af26babf1d93727cf6a32be2fd66806bde3e22
Fernando Rivera <fernando.rivera0011@gmail.com>
2023-01-31 17:29:57 -0600

    Finished promises Exercice

--- js/promises.js
+++ js/promises.js
@@ -1,43 +1,12 @@
-// (async () => {
-//
-//
-//     async function getUserAsync(username) {
-//         try {
-//             let response = await fetch(`https://api.github.com/users/${username}/events`, {headers: {'Authorization': `token ${keys.github}`}})
-//             // console.log(response)
-//             let data = await response.json();
-//             // console.log(data)
-//             return data;
-//         } catch (error) {
-//             console.log(error)
-//         }
-//     }
-//
-//     async function lastCommit(username) {
-//         try {
-//             let response = await getUserAsync(username);
-//             // console.log(response)
-//             let commit = response[0].created_at;
-//             // console.log(commit)
-//             return commit;
-//         } catch (error) {
-//             console.log(error);
-//         }
-//     }
-//
-//     let answer = await lastCommit(`calirex`);
-//     console.log(answer)
-//
-// })();
+(async () => {
 
-(async ()=> {
 
     async function getUserAsync(username) {
         try {
-            let response = await fetch(`https//:api.github.com/users/${userName}/events`, {headers: {`Authorization`: `token ${keys.github}`}})
-            //console.log(response)
+            let response = await fetch(`https://api.github.com/users/${username}/events`, {headers: {'Authorization': `token ${keys.github}`}})
+            // console.log(response)
             let data = await response.json();
-            //console.log(data)
+            // console.log(data)
             return data;
         } catch (error) {
             console.log(error)
@@ -47,10 +16,16 @@
     async function lastCommit(username) {
         try {
             let response = await getUserAsync(username);
-            //console.log(commit)
+            // console.log(response)
+            let commit = response[0].created_at;
+            // console.log(commit)
             return commit;
         } catch (error) {
-            console.log(error)
+            console.log(error);
+        }
     }
-    }
-});
+
+    let answer = await lastCommit(`calirex`);
+    console.log(answer)
+
+})();
